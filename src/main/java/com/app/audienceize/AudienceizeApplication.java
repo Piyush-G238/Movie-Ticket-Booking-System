3f84c34d71310d6ecfd563ce551e58cae655ba24
Piyush Gupta <guptapiyush238@gmail.com>
2023-04-16 00:30:34 +0530

    Project initialized

---
+++ src/main/java/com/app/audienceize/AudienceizeApplication.java
@@ -0,0 +1,13 @@
+package com.app.audienceize;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AudienceizeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AudienceizeApplication.class, args);
+	}
+
+}
